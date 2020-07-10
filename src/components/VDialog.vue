c122c74d006afbfcd773bcb1bb5e735f1179faec
Adam Whitlock <awhitlock@dealerinspire.com>
2020-07-09 23:22:12 -0600

    initial commit

---
+++ src/components/VDialog.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>dialog here</p>
+    <p v-if="title">{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VDialog",
+  props: {
+    title: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
